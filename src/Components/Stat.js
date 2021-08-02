7257a35e046d6e164695d13286a5f3fd28372b81
Dhekra Nsir <dhekransir@gmail.com>
2021-08-02 13:11:40 +0100

    state

--- src/Components/Stat.js
+++ src/Components/Stat.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import photo from '../Components/photo.jpg';
 class stat extends Component { 
     constructor(props){
+        // eslint-disable-next-line
         super(props)
         this.state={ person:{
             fullName: 'Dhekra Nsir', 
@@ -9,13 +10,19 @@ class stat extends Component {
             bio:'I am a 29-year-old married woman. I live in Dar Chaabane,Nabeul,Tunisia.I studied Agronomy studies at ISA Sousse where I graduated as an engineer. I have decided to switch to a different career path, namely web development.Hence, I have signed up to the GoMyCode fullstack program in order to acquire a deeper understanding of this field of study.',
             profession:'Web Developer'}, 
             show:true,
-            date: new Date()
+            date: 0,
+            timer:0
      }
     }
     handleClick=()=>this.setState({show:!this.state.show})
-    count=()=>this.setState({date: new Date() });
     componentDidMount() {
-        setInterval(() => this.count(), 1000 );
+        this.setState({
+            date:setInterval(()=>{
+                this.setState({timer:this.state.timer+1});
+            },1000),
+
+        });
+       
       }
     
       
@@ -31,8 +38,8 @@ class stat extends Component {
                 </div>:<div></div>}
                 <button style={{backgroundColor:'darkmagenta',border: 'none',color: 'white',padding: '15px 32px',
   textAlign: 'center',fontSize: 16,margin: '4px 2px' ,cursor: 'pointer'}} onClick={this.handleClick}>Profile</button>
-
-            </div>
+<h2>{this.state.timer}</h2>
+</div>
 
         )
     }  
